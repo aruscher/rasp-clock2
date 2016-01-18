f245dc3fb9b4939a961c0e35d62928b9d98cb889
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2016-01-18 18:49:48 +0100

    add tick

--- raspclock/main.py
+++ raspclock/main.py
@@ -3,6 +3,7 @@ import logging
 import logging.handlers
 import sys
 import stage
+import Queue
 
 
 
@@ -12,15 +13,21 @@ class ClockApp():
 
     def start(self):
         self.__configure_logging()
-        self.logger.debug("Start Clock App")
-        zmpstage = stage.ReadZMPStage()
+        self.logger.info("Start Clock App")
+        queue =  Queue.Queue()
+        zmpstage = stage.ReadZMPStage(queue)
         zmp = zmpstage.run()
-        rtstage = stage.ReadTimesStage(zmp)
+        rtstage = stage.ReadTimesStage(queue,zmp)
         time_offsets = rtstage.run()
-        ctstage = stage.CheckTimesStage(time_offsets)
+        ctstage = stage.CheckTimesStage(queue,time_offsets)
         checked_time_offsets = ctstage.run()
-        wfsstage = stage.WaitForStartStage()
-
+        self.logger.info("Clock Configuration completed")
+        self.logger.info("Zeitmesspunkte: %d"%zmp)
+        self.logger.info("Zeitoffsets: %s"%[ctstage.timedelta_to_str(offset) for offset in checked_time_offsets])
+        wfsstage = stage.WaitForStartStage(queue)
+        wfsstage.run()
+        crstage = stage.ClockRunningStage(queue,checked_time_offsets)
+        crstage.run()
 
 
     def __configure_logging(self):
--- raspclock/stage.py
+++ raspclock/stage.py
@@ -3,12 +3,13 @@ import Adafruit_CharLCD as LCD
 import sys
 import tty
 import termios
-from datetime import timedelta
+from datetime import timedelta,datetime
 import threading
-import Queue
+from math import floor
+import time
 
 class LCDKeyThrad(threading.Thread):
-    def __init__(self, queue,lcd,logger):
+    def __init__(self, queue, lcd, logger):
         super(LCDKeyThrad, self).__init__()
         self.queue = queue
         self.lcd = lcd
@@ -18,19 +19,19 @@ class LCDKeyThrad(threading.Thread):
         self.logger.debug("RUN LCD KEY THREAD")
         while True:
             key = self.read_lcd_key()
-            self.logger.debug("Put %s to Key-Queue"%key)
+            self.logger.debug("Put %s to Key-Queue" % key)
             self.queue.put(key)
 
     def read_lcd_key(self):
         pressed_key = None
-        keys = [LCD.SELECT,LCD.LEFT,LCD.RIGHT,LCD.UP,LCD.DOWN]
+        keys = [LCD.SELECT, LCD.LEFT, LCD.RIGHT, LCD.UP, LCD.DOWN]
         # read pressed key
         self.logger.debug("Read Pressed Key")
-        while pressed_key is None :
+        while pressed_key is None:
             for key in keys:
                 if self.lcd.is_pressed(key):
                     pressed_key = key
-        self.logger.debug("%s Key was read"%pressed_key)
+        self.logger.debug("%s Key was read" % pressed_key)
         # wait until key is released
         self.logger.debug("Wait for releasing of key")
         is_released = False
@@ -40,15 +41,24 @@ class LCDKeyThrad(threading.Thread):
         return pressed_key
 
 
+class TickThread(threading.Thread):
+    def __init__(self,queue):
+        super(TickThread, self).__init__()
+        self.queue = queue
+
+    def run(self):
+        while True:
+            self.queue.put(8)
+            time.sleep(1)
+
 class Stage():
-    def __init__(self):
+    def __init__(self,queue):
         self.logger = logging.getLogger("logger")
         self.lcd = LCD.Adafruit_CharLCDPlate()
         self.first_line = ""
         self.second_line = ""
         self.lcd_key_thread = None
-        self.communication_queue = Queue.Queue()
-
+        self.communication_queue = queue
 
     def run(self):
         raise NotImplementedError
@@ -56,7 +66,7 @@ class Stage():
     def write_msg_to_display(self, msg):
         self.lcd.clear()
         self.lcd.message(msg)
-        self.logger.debug("write '%s' to display"%msg.replace("\n","\\n"))
+        self.logger.debug("write '%s' to display" % msg.replace("\n", "\\n"))
 
     def write_memory_to_display(self):
         msg = "%s\n%s" % (self.first_line, self.second_line)
@@ -73,59 +83,58 @@ class Stage():
         return ch
 
     def _read_time(self):
-        time = [0,0,0,0]
-        self.second_line = self._time_to_str(time)
+        time = [0, 0, 0, 0]
+        self.second_line = self.time_to_str(time)
         self.write_memory_to_display()
         self.lcd.blink(True)
-        #read every diget of the time
-        cursor_positions = [0,1,3,4]
+        # read every diget of the time
+        cursor_positions = [0, 1, 3, 4]
         counter = 0
         while True:
-            #move cursor to corresponding number
-            self.lcd.set_cursor(cursor_positions[counter],1)
-            #read until valid input
+            # move cursor to corresponding number
+            self.lcd.set_cursor(cursor_positions[counter], 1)
+            # read until valid input
             while True:
                 key = self.get_char_from_keyboard()
-                #accpet only numeric imput
+                # accpet only numeric imput
                 if 48 <= ord(key) <= 57:
                     time[counter] = int(key)
                     counter += 1
                     break
-                #backspace for removing last input
+                # backspace for removing last input
                 elif ord(key) == 127:
-                    time[counter-1] = 0
+                    time[counter - 1] = 0
                     counter -= 1
                     break
-                #ignore other input
+                # ignore other input
                 else:
                     continue
-            self.second_line = self._time_to_str(time)
+            self.second_line = self.time_to_str(time)
             self.write_memory_to_display()
             if counter == len(cursor_positions):
                 break
         return time
 
-    def _convert_time_to_offset(self,time):
-        minutes = int("%d%d"%(time[0],time[1]))
-        seconds = int("%d%d"%(time[2],time[3]))
-        delta = timedelta(minutes=minutes,seconds=seconds)
+    def convert_time_to_offset(self, time):
+        minutes = int("%d%d" % (time[0], time[1]))
+        seconds = int("%d%d" % (time[2], time[3]))
+        delta = timedelta(minutes=minutes, seconds=seconds)
         return delta
 
-    def _time_to_str(self,time):
-        return "%d%d:%d%d"%(time[0],time[1],time[2],time[3])
+    def time_to_str(self, time):
+        return "%d%d:%d%d" % (time[0], time[1], time[2], time[3])
 
-    def _timedelta_to_str(self,time):
+    def timedelta_to_str(self, time):
         time_text = str(time)
         splitted_time_text = time_text.split(":")
-        splitted_time_ints = [int(elem) for elem in splitted_time_text]
-        time_converted = [splitted_time_ints[0]*60+splitted_time_ints[1],splitted_time_ints[2]]
-        return "%02d:%02d"%(time_converted[0],time_converted[1])
-
+        splitted_time_ints = [int(floor(float(elem))) for elem in splitted_time_text]
+        time_converted = [splitted_time_ints[0] * 60 + splitted_time_ints[1], splitted_time_ints[2]]
+        return "%02d:%02d" % (time_converted[0], time_converted[1])
 
 
 class ReadZMPStage(Stage):
-    def __init__(self):
-        Stage.__init__(self)
+    def __init__(self,queue):
+        Stage.__init__(self,queue)
         self.logger.debug("Read ZMP Stage created")
         self.zmp = 0
 
@@ -143,7 +152,7 @@ class ReadZMPStage(Stage):
         while (True):
             key = self.get_char_from_keyboard()
             # Enter
-            if ord(key) == 13:
+            if ord(key) == 13 or ord(key) == 102:
                 break
             # backspace
             elif ord(key) == 127:
@@ -156,30 +165,30 @@ class ReadZMPStage(Stage):
         self.zmp = int(text)
         self.logger.debug("Read '%d' Zeitmesspunkte" % self.zmp)
 
+
 class ReadTimesStage(Stage):
-    def __init__(self, zmp):
-        Stage.__init__(self)
+    def __init__(self,queue, zmp):
+        Stage.__init__(self,queue)
         self.logger.debug("RT-Stage created")
         self.zmp = zmp
         self.time_offsets = []
 
     def run(self):
         self.logger.debug("Run RT-Stage")
-        for current_index in range(0,self.zmp):
-            self.logger.debug("Read Timer: %d"%(current_index+1))
-            self.first_line = "Timer %d:"%(current_index+1)
+        for current_index in range(0, self.zmp):
+            self.logger.debug("Read Timer: %d" % (current_index + 1))
+            self.first_line = "Timer %d:" % (current_index + 1)
             time = self._read_time()
-            self.logger.debug("Read Timer: %s"%self._time_to_str(time))
-            time_offset = self._convert_time_to_offset(time)
-            self.logger.debug("Convert Timer to Offset: %s hh:mm:ss"%time_offset)
+            self.logger.debug("Read Timer: %s" % self.time_to_str(time))
+            time_offset = self.convert_time_to_offset(time)
+            self.logger.debug("Convert Timer to Offset: %s hh:mm:ss" % time_offset)
             self.time_offsets.append(time_offset)
         return self.time_offsets
 
 
-
 class CheckTimesStage(Stage):
-    def __init__(self,offsets):
-        Stage.__init__(self)
+    def __init__(self, queue,offsets):
+        Stage.__init__(self,queue)
         self.logger.debug("CT-Stage created")
         self.offsets = offsets
 
@@ -195,38 +204,77 @@ class CheckTimesStage(Stage):
             if pressed_key == LCD.SELECT:
                 self.check_times()
                 break
-        self.logger.debug("Complete timer controll")
-        self.logger.debug("Saved Timers: %s"%[self._timedelta_to_str(offset) for offset in self.offsets])
+        self.logger.debug("Complete timer control")
+        self.logger.debug("Saved Timers: %s" % [self.timedelta_to_str(offset) for offset in self.offsets])
         return self.offsets
-
 
     def check_times(self):
         current_index = 0
-        self.communication_queue.queue.clear()
         is_ready = False
         while not is_ready:
             current_offset = self.offsets[current_index]
-            self.first_line = "Timer %d"%(current_index+1)
-            self.second_line = self._timedelta_to_str(current_offset)
+            self.first_line = "Timer %d" % (current_index + 1)
+            self.second_line = self.timedelta_to_str(current_offset)
             self.write_memory_to_display()
             pressed_key = self.communication_queue.get()
-            if pressed_key == LCD.UP and current_index != len(self.offsets)-1:
+            if pressed_key == LCD.UP and current_index != len(self.offsets) - 1:
                 current_index += 1
                 self.logger.debug("Show next timer")
             elif pressed_key == LCD.DOWN and current_index > 0:
-                current_index -=1
+                current_index -= 1
                 self.logger.debug("Show previous timer")
             elif pressed_key == LCD.RIGHT:
                 self.logger.debug("Reread timer")
-                time = self._convert_time_to_offset(self._read_time())
-                self.logger.debug("Change timer from %s to %s"%(self.offsets[current_index],time))
+                time = self.convert_time_to_offset(self._read_time())
+                self.logger.debug("Change timer from %s to %s" % (self.offsets[current_index], time))
                 self.offsets[current_index] = time
                 self.lcd.blink(False)
             elif pressed_key == LCD.LEFT:
                 return
 
+
 class WaitForStartStage(Stage):
-    def __init__(self):
-        Stage.__init__(self)
+    def __init__(self,queue):
+        Stage.__init__(self,queue)
         self.logger.debug("WFS-Stage created")
 
+    def run(self):
+        self.logger.debug("WFS-Stage run")
+        self.first_line = "Bereit"
+        self.second_line = ""
+        self.write_memory_to_display()
+        while True:
+            key = self.communication_queue.get()
+            self.logger.debug("pressed key")
+            if key == LCD.RIGHT:
+                break
+        self.logger.debug("Complete WFS Stage")
+
+
+class ClockRunningStage(Stage):
+    def __init__(self,queue, offsets):
+        Stage.__init__(self,queue)
+        self.logger.debug("CRS-Stage created")
+        self.offsets = offsets
+        self.reference_point = datetime.now()
+        self.logger.info("Timer reference point: %s"%self.reference_point)
+
+    def run(self):
+        self.logger.debug("CRS-Stage run")
+        current_index = 0
+        self.first_line = "Timer %d"%(current_index+1)
+        self.write_memory_to_display()
+        ticker = TickThread(self.communication_queue)
+        ticker.start()
+        while True:
+            event = self.communication_queue.get()
+            if event == 8:
+                rest_time = self.calculate_remaining_time(self.offsets[0])
+                self.second_line = self.timedelta_to_str(rest_time)
+                self.write_memory_to_display()
+        return
+
+    def calculate_remaining_time(self,offset):
+        end_date = self.reference_point + offset
+        rest_time = end_date-datetime.now()
+        return rest_time
